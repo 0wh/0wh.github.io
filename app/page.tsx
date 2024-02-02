8a4e2382d5b03ce41e2f5704219cb04287a7dacb
何偉銘 <wh249@njit.edu>
2024-02-02 00:00:28 -0500

    optimize appearance

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,22 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center py-5 sm:p-5 sm:pt-16 lg:p-32 lg:pt-16">
       <div className="mb-4 max-w-[600px] lg:max-w-[900px] xl:max-w-[1200px] w-full">
         <div className="flex items-center">
+          <div className="sm:hidden z-20 absolute translate-y-14 mb-2 ml-6 w-20 h-20 rounded-[40px] bg-gray-100 dark:bg-black" />
           <Image
-            className="absolute translate-y-14 sm:static sm:translate-y-0 mb-2 ml-6 sm:ml-10 mr-4 rounded-[40px] border-2 border-white/30 dark:border-white/60 p-0.5"
+            className="z-20 absolute translate-y-14 sm:static sm:translate-y-0 mb-2 ml-6 sm:ml-10 mr-4 rounded-[40px] border-2 border-black/10 dark:border-white/60 p-0.5"
             src="/photo.png"
             alt="NextGPT.live"
             width={80}
             height={80}
             priority
           />
-          <p className="mx-4 text-center sm:text-start font-sans">
+          <p className="mx-4 text-center sm:text-start font-sans max-w-[500px] sm:max-w-none">
             Hello! I'm{" "}
             <span className="font-semibold font-serif text-2xl">
               Mike Hyatt
             </span>
-            . Please feel free to converse with my AI assistant. They're here to
-            help!
+            . Please feel free to converse with my AI assistant.
+            <span className="hidden sm:inline"> It's here to help!</span>
           </p>
         </div>
         <div className="sm:mx-4 sm:rounded-lg border border-transparent my-4 px-5 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30">
